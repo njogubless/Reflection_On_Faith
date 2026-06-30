d86da429266e10287df4263f5a3e0dde6e26a42c
njogubless <pnjogubless@gmail.com>
2026-06-30 09:59:35 +0300

    fix(safety):Future.delayed callback and if return; a before reading FirebaseAuth.instance.currentUser

--- lib/features/auth/presentation/screen/splash_screen.dart
+++ lib/features/auth/presentation/screen/splash_screen.dart
@@ -1,10 +1,8 @@
-
 import 'package:devotion/features/auth/presentation/screen/login_screen.dart';
 import 'package:devotion/features/auth/presentation/screen/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
@@ -12,14 +10,13 @@ class SplashScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Future.delayed(const Duration(seconds: 3), () {
+      if (!context.mounted) return;
       final user = FirebaseAuth.instance.currentUser;
       if (user != null) {
-     
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => LoginScreen()),
         );
       } else {
-       
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const SignUpScreen()),
         );
