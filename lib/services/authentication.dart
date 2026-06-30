5e17cd0edefefd7f269fc94a60469d8d3853adbf
njogubless <pnjogubless@gmail.com>
2026-06-30 10:45:01 +0300

    chore(lint): - Remove unused _firestore field, _userModel field, _users getter,Remove UserModel and cloud_firestore imports (re-order remaining imports)

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,21 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:devotion/features/auth/data/models/user_models.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
-  final FirebaseFirestore _firestore;
   final FirebaseAuth _firebaseAuth;
 
   AuthService(
       {required FirebaseFirestore firestore,
       required FirebaseAuth firebaseAuth})
-      : _firestore = firestore,
-        _firebaseAuth = firebaseAuth;
+      : _firebaseAuth = firebaseAuth;
 
-  late UserModel _userModel;
-  CollectionReference get _users => _firestore.collection('users');
-
-  Stream<User?>get authStateChange => _firebaseAuth.authStateChanges();
-  
-   
+  Stream<User?> get authStateChange => _firebaseAuth.authStateChanges();
 }
